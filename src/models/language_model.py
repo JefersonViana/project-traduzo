0c483f41832a66777b8db11fc8e963a1d30a2f54
jeferson-viana <jefersonv28@gmail.com>
2023-11-21 18:37:48 -0300

    FEAT: Criando os métodos de LanguageModel

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,4 +1,4 @@
-from abstract_model import AbstractModel
+from .abstract_model import AbstractModel
 from database.db import db
 
 
@@ -7,3 +7,16 @@ class LanguageModel(AbstractModel):
 
     def __init__(self, data: dict[str, str]):
         super().__init__(data)
+
+    def to_dict(self) -> dict[str, str]:
+        return {
+            "name": self.data["name"],
+            "acronym": self.data["acronym"],
+        }
+
+    @classmethod
+    def list_dicts(cls) -> list[dict[str, str]]:
+        return [
+            language.to_dict()
+            for language in LanguageModel.find()
+        ]
